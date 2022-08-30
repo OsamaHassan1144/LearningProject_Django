32446b91337442c4ff034ec2fc8018f990850da0
OsamaHassan1144 <osamahassan1144@gmail.com>
2022-08-30 19:43:22 +0500

    adding parcipates in model

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -50,7 +50,6 @@ def registerPage(request):
             messages.error(request, 'An error occured during registration')
     return render(request,'base/login_register.html',context)
 
-
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q)| Q(description__icontains=q)) 
