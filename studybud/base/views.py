2dceb9284e5eb7240aa7f209b94736e4c2efc95e
OsamaHassan1144 <osamahassan1144@gmail.com>
2022-09-09 17:39:34 +0500

    mob. responsive

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -57,7 +57,7 @@ def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(
         name__icontains=q) | Q(description__icontains=q))
-    topic = Topic.objects.all()
+    topic = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)).order_by('-created')
     context = {'rooms': rooms, 'topics': topic, 'room_count': room_count, 'room_messages':room_messages}
@@ -81,6 +81,7 @@ def room(request, pk):
                'participants': participants}
     return render(request, 'base/room.html', context)
 
+
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
@@ -88,6 +89,18 @@ def userProfile(request, pk):
     topics = Topic.objects.all()
     context={'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics  }
     return render(request, 'base/profile.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = { 'topics': topics }
+    return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all().order_by('-created')
+    context = { 'room_messages': room_messages }
+    return render(request, 'base/activity.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
